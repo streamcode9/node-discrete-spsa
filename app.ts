a93932507134959a75ee3f97de285622c1c24c97
streamcode9@gmail.com <streamcode9@gmail.com>
2016-05-14 00:53:53 +0300

    Internal: Build with implicitAny Fixes #1

--- app.ts
+++ app.ts
@@ -2,11 +2,11 @@
 
 function bernoulli() { return Math.random() > 0.5 ? 1 : -1 }
 
-function vector_add(a, b) { return a.map((ai, i) => ai + b[i]) }
-function vector_sub(a, b) { return a.map((ai, i) => ai - b[i]) }
-function scale(a, n) { return a.map((x) => x * n) }
+function vector_add(a: number[], b: number[]) { return a.map((ai, i) => ai + b[i]) }
+function vector_sub(a: number[], b: number[]) { return a.map((ai, i) => ai - b[i]) }
+function scale(a: number[], n: number) { return a.map((x) => x * n) }
 
-export function iterationSync(f, theta, a) {
+export function iterationSync(f: (values: number[]) => number, theta: number[], a: number) {
 	var delta = theta.map(bernoulli)
 	var gradient = scale( delta, (f(vector_add(theta, delta)) - f(vector_sub(theta, delta)) / 2) * a)
 	return vector_sub(theta, gradient)
