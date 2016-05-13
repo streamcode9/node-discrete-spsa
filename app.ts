6fd6c32fee25adb00138a55cc78779e93f3766d5
Andrii Melnykov <andy.melnikov@gmail.com>
2016-05-14 00:37:08 +0300

    Internal: initial version based on paper

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-﻿
+﻿// Main library module
+
+function bernoulli() { return Math.random() > 0.5 ? 1 : -1 }
+
+function vector_add(a, b) { return a.map((ai, i) => ai + b[i]) }
+function vector_sub(a, b) { return a.map((ai, i) => ai - b[i]) }
+function scale(a, n) { return a.map((x) => x * n) }
+
+export function iterationSync(f, theta, a) {
+	var delta = theta.map(bernoulli)
+	var gradient = scale( delta, (f(vector_add(theta, delta)) - f(vector_sub(theta, delta)) / 2) * a)
+	return vector_sub(theta, gradient)
+}
