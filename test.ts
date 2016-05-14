6b826c0383f1f73bacd71e49a9cd33bac9d86bf8
streamcode9@gmail.com <streamcode9@gmail.com>
2016-05-14 21:54:04 +0300

    Internal: Basic vs-mocha test suite Fixes #2

--- test.ts
+++ test.ts
@@ -1,21 +1,15 @@
 import assert = require('assert')
 import lib = require('./app')
 
-function test()
-{
-	var theta = [0]
-	for (var i = 1; i < 25; i++)
-	{
-		theta = lib.iterationSync(x => Math.sin(x[0] / 3), theta, -1.3 * 10 / i).map((x => Math.round(x)))
-	}
-	assert.ok(theta[0] == 4 || theta[0] == 5)
-}
+describe("Test", () => {
+    it("1d optimization of x => sin(x / 100) should converge", () => {
+		var theta = [0]
+		for (var i = 1; i < 25; i++)
+		{
+			theta = lib.iterationSync(x => Math.sin(x[0] / 3), theta, -1.3 * 10 / i).map((x => Math.round(x)))
+		}
 
-for (var i = 0; i < 10; i++)
-{
-	console.log(i, Math.sin(i / 3).toFixed(2))
-}
-
-test()
-console.log('test ok')
+        assert.ok(theta[0] == 4 || theta[0] == 5)
+    })
+})
 
