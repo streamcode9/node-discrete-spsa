b407b63f242c3a2c06c23596b7806555f7b592b7
streamcode9@gmail.com <streamcode9@gmail.com>
2016-05-15 13:08:36 +0300

    Promise-based asynchronous iterate Fixes #10

--- app.ts
+++ app.ts
@@ -11,3 +11,14 @@ export function iterationSync(f: (values: number[]) => number, theta: number[], 
 	var gradient = scale(delta, (f(vector_add(theta, delta)) - f(vector_sub(theta, delta))) / 2 * a)
 	return vector_sub(theta, gradient)
 }
+
+export function iteration(f: (...args: any[]) => Promise<number>, theta: number[], a: number) : Promise<number[]> {
+	var delta = theta.map(bernoulli)
+	var ysPromises = [f.apply(null, [vector_add(theta, delta)]), f.apply(null, [vector_sub(theta, delta)])]	
+	return new Promise<number[]>(function(resolve) {
+		Promise.all(ysPromises).then(function(ys) {
+			var gradient = scale(delta, (ys[0] - ys[1]) / 2 * a)
+			resolve(vector_sub(theta, gradient))
+		})
+	})
+}
--- test.ts
+++ test.ts
@@ -3,13 +3,31 @@ import lib = require('./app')
 
 describe("Test", () => {
     it("1d optimization of x => sin(x / 100) should converge", () => {
-		var theta = [0]
+		let theta = [0]
 		for (var i = 1; i < 25; i++)
 		{
 			theta = lib.iterationSync(x => Math.sin(x[0] / 3), theta, -1.3 * 10 / i).map((x => Math.round(x)))
 		}
 
         assert.ok(theta[0] == 4 || theta[0] == 5)
+    })
+
+	it("async 1d optimization of x => sin(x / 100) should converge", done => {
+		let thetaPromise : Promise<number[]>
+
+		function run(i = 1, theta = [0]) {
+			thetaPromise = lib.iteration(x => new Promise<number>(resolve => resolve(Math.sin(x[0] / 3))), theta, -1.3 * 10 / i)
+			thetaPromise.then(t => {
+				let thetaNext = t.map((x => Math.round(x)))
+				if (i < 25) run(i + 1, thetaNext)
+				else {
+					assert.ok(thetaNext[0] == 4 || thetaNext[0] == 5)
+					done()
+				}
+			})
+		}
+		
+		run()        
     })
 })
 
